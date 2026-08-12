f9206932922e93505f24209b8e166ce280189eb9
Vishal <vishalchandel0987@gmail.com>
2026-08-12 15:44:28 +0530

    feat: add stub files for unimplemented Maxflow-Mincut buddy task

---
+++ assignment_03/src/maxflow_mincut.cpp
@@ -0,0 +1,7 @@
+#include "maxflow_mincut.h"
+#include <iostream>
+
+void MaxflowMincutSolver::solve(const std::string& filepath) {
+    (void)filepath;
+    std::cout << "Algorithm: Maxflow-Mincut is not implemented in this project.\n";
+}
---
+++ assignment_03/src/maxflow_mincut.h
@@ -0,0 +1,11 @@
+#ifndef MAXFLOW_MINCUT_H
+#define MAXFLOW_MINCUT_H
+
+#include <string>
+
+class MaxflowMincutSolver {
+public:
+    static void solve(const std::string& filepath);
+};
+
+#endif
